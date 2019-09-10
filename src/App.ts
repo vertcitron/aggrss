01c7389cfa890d092c1b8cb23c69ce5e4a8d8167
Stéphane Souron <stef@vertcitron.fr>
2019-09-10 13:29:17 +0200

    persistence enabled

--- src/App.ts
+++ src/App.ts
@@ -11,15 +11,6 @@ const aggregator = new Aggregator()
 
 const promises = [
   aggregator.getFromURL(new URL('https://hnrss.org/newest')),
-  aggregator.getFromURL(new URL('https://www.google.fr')),
   aggregator.getFromURL(new URL('http://feeds.bbci.co.uk/news/world/europe/rss.xml')),
-  aggregator.getFromURL(new URL('https://hnrss.org/newest')),
-  aggregator.getFromURL(new URL('https://nonexistingweb.site')),
   aggregator.getFromURL(new URL('https://css-tricks.com/feed/'))
 ].map(p => p.catch(e => e)) // trick to polyfill Promise.allSettled
-
-Promise.all(promises)
-  .then(() => {
-    aggregator.remove('http://feeds.bbci.co.uk/news/world/europe/rss.xml')
-    aggregator.remove('https://www.google.com')
-  })
--- src/models/Aggregator.ts
+++ src/models/Aggregator.ts
@@ -4,9 +4,13 @@ import Feed from './Feed'
 import { RawFeed } from './RssRawData'
 
 export default class Aggregator {
-  readonly feeds: Feed[] = []
+  public feeds: Feed[] = []
 
-  public getFromURL (url: URL): Promise<Feed> {
+  constructor () {
+    this.restore()
+  }
+
+  public getFromURL (url: URL, selected: boolean = true): Promise<Feed> {
     const rss2json = 'https://api.rss2json.com/v1/api.json?rss_url='
     return fetch(encodeURI(rss2json + url.href))
       .then(response => {
@@ -20,9 +24,10 @@ export default class Aggregator {
         if (this.hasFeed(feed)) {
           throw new Error('That feed has already been added.')
         }
-        feed.selected = true
+        feed.selected = selected
         this.feeds.push(feed)
         console.log(`Added new Feed @ ${feed.url.href}`)
+        this.save()
         this.log()
         return Feed
       })
@@ -53,10 +58,30 @@ export default class Aggregator {
    if (index > -1) {
       this.feeds.splice(index, 1)
       console.log(`Removed feed ${url}`)
+      this.save()
       this.log()
     } else {
       console.error(`Error when removing feed ${url} :`)
       console.error('This feed was not in the aggregator.')
+    }
+  }
+
+  private save () {
+    const data = this.feeds.map(f => ({ url: f.url.href, selected: f.selected }))
+    if (data.length) {
+      localStorage.setItem('feeds', JSON.stringify(data))
+    } else {
+      localStorage.removeItem('feeds')
+    }
+  }
+
+  private restore () {
+    const rawData = localStorage.getItem('feeds')
+    if (rawData) {
+      this.feeds = []
+      for (const item of JSON.parse(rawData)) {
+        this.getFromURL(new URL(item.url), item.selected)
+      }
     }
   }
 }
