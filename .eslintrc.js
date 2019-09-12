5fcd4e133c8c8ab4bf79ddb53d15e6fbe9f78a99
Stéphane Souron <stef@vertcitron.fr>
2019-09-12 20:25:19 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+    
+  },
+}
---
+++ src/App.ts
@@ -0,0 +1,5 @@
+import '../style/main.sass'
+
+const title = document.createElement('h1')
+title.textContent = 'AGGRSS!';
+document.body.appendChild(title)
