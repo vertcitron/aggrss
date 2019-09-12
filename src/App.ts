5d7319c99458ebf80dfb41a077e3b95ce731c02a
Stéphane Souron <stef@vertcitron.fr>
2019-09-12 21:29:53 +0200

    styling base

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import '../style/main.sass'
 
+// temporary DOM insertion to check if everything works
 const title = document.createElement('h1')
 title.textContent = 'AGGRSS!'
 document.body.appendChild(title)
